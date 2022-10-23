5ec99a868287b1d1b94339eaf8cb12694df05a24
Luca graves <lgrave264@west-mec.org>
2022-10-23 14:26:53 -0700

    finished

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// this function will alert the start message when the start button is clicked
+function start(){
+    alert("Started")
+}
+
+// this function will alert the stop message when the stop button is clicked
+function stop(){
+    alert("Stopped")
+}
